31344e5b1f0d311ca3fba586546e10a6e765dab8
Alex Volkov <alex@flamy.ca>
2013-04-13 19:15:23 -0400

    grok.View setUpWidgets method.

--- src/todosimple/app.py
+++ src/todosimple/app.py
@@ -2,6 +2,12 @@ import grok
 from todosimple import resource
 from zope import interface, schema
 
+def setup_widgets(self, ignore_request=False):
+    super(self.__class__, self).setUpWidgets(ignore_request)
+    self.widgets['title'].displayWidth = 50
+    self.widgets['description'].height = 5
+
+
 class Todo(grok.Application, grok.Container):
     def __init__(self):
         super(Todo, self).__init__()
@@ -29,12 +35,14 @@ class Project(grok.Container):
     def deleteList(self, list):
         del self[list]
 
+
+
 class AddProjectForm(grok.AddForm):
     grok.context(Todo)
     grok.name('index')
     form_fields = grok.AutoFields(IProject).omit('next_id')
     label = "To begin, add a new project"
-
+    setUpWidgets = setup_widgets
     @grok.action('Add Project')
     def add(self, **data):
         project = Project()
@@ -44,6 +52,13 @@ class AddProjectForm(grok.AddForm):
         self.context[id] = project
         new_url = self.url(self.context[id])
         return self.redirect(new_url)
+
+class EditProjectForm(grok.EditForm):
+    grok.context(Project)
+    grok.name('edit')
+    form_fields = grok.AutoFields(Project).omit('next_id')
+    label = "Edit the project"
+    setUpWidgets = setup_widgets
 
 class ProjectView(grok.View):
     grok.context(Project)
